7b0144da5d4c33bb7a30e69bdd39b84462f3eb7d
Danil Ternovykh <danil.ternovykh@jibrel.network>
2020-04-02 22:59:02 +0300

    home work

--- app/src/main/java/net/palmut/speedometer/MainActivity.kt
+++ app/src/main/java/net/palmut/speedometer/MainActivity.kt
@@ -2,10 +2,35 @@ package net.palmut.speedometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        buttonA.setOnClickListener {
+            Toast.makeText(this, R.string.button_a_text, Toast.LENGTH_SHORT).show()
+        }
+        buttonB.setOnClickListener {
+            Toast.makeText(this, R.string.button_b_text, Toast.LENGTH_SHORT).show()
+        }
+        buttonC.setOnClickListener {
+            Toast.makeText(this, R.string.button_c_text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
 }
